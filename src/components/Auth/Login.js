96f193ba7e03cf04a3c1662098b53ad475df3d54
Paus Haus <48162083+PDunCanCode@users.noreply.github.com>
2020-01-21 15:09:07 -0800

    Add files via upload

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import useFormValidation from "./useFormValidation";
 import validateLogin from "./validateLogin";
+import firebase from '../../firebase';
 
 const INITIAL_STATE = {
   name: "",
@@ -16,8 +17,22 @@ function Login(props) {
     values,
     errors,
     isSubmitting
-  } = useFormValidation(INITIAL_STATE, validateLogin);
+  } = useFormValidation(INITIAL_STATE, validateLogin, authenticateUser);
   const [login, setLogin] = React.useState(true);
+  const [firebaseError, setFirebaseError] = React.useState(null);
+
+  async function authenticateUser() {
+    const { name, email, password } = values;
+    try {
+      login
+    ? await firebase.login(email, password)
+    : await firebase.register(name, email, password);
+    } catch (err) {
+      console.error('Authentication Error', err)
+      setFirebaseError(err.message)
+    }
+  }
+
 
   return (
     <div>
@@ -54,6 +69,7 @@ function Login(props) {
           placeholder="Choose a secure password"
         />
         {errors.password && <p className="error-text">{errors.password}</p>}
+        {firebaseError && <p className="error-text">{firebaseError}</p>}
         <div className="flex mt3">
           <button
             type="submit"
